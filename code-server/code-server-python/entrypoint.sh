a4b73bea665c0446b06260263c29ba1dd7d29753
Scott Seo <scott.seo@paramount.com>
2024-08-25 17:54:13 -0400

    updating python code server

--- code-server/code-server-python/entrypoint.sh
+++ code-server/code-server-python/entrypoint.sh
@@ -17,13 +17,4 @@ export DEFAULT_VALUE="vscode-${VSCODE_USERNAME}.curiosityworks.org"
 
 export VSCODE_HOST=${VSCODE_HOST:-"${DEFAULT_VALUE}"}
 
-cat << EOF > /home/coder/.local/bin/gotty.sh
-#!/bin/sh
-export TERM=xterm
-/usr/local/bin/gotty --ws-origin "${VSCODE_HOST}" -p 2222 -c "${VSCODE_USERNAME}:${VSCODE_PASSWORD}" -w /usr/bin/zsh >>/dev/null 2>&1 
-EOF
-chmod +x /home/coder/.local/bin/gotty.sh
-nohup /home/coder/.local/bin/gotty.sh &
-
-
 dumb-init /usr/bin/code-server --bind-addr 0.0.0.0:9991 /home/coder/workspace
